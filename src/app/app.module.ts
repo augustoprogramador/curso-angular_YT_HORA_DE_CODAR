1d14e9b52fe252d94e86f743c4f1d370ece5aa40
augustoprogramador <augusto.camargo@aluno.ifsp.edu.br>
2023-01-09 22:51:57 -0300

    exemplos de pipe operators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { EventosComponent } from './components/eventos/eventos.component';
 import { EmissorComponent } from './components/emissor/emissor.component';
 import { ChangeNumberComponent } from './components/change-number/change-number.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
+import { PipesComponent } from './components/pipes/pipes.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { ListRenderComponent } from './components/list-render/list-render.compon
     EventosComponent,
     EmissorComponent,
     ChangeNumberComponent,
-    ListRenderComponent
+    ListRenderComponent,
+    PipesComponent
   ],
   imports: [
     BrowserModule,
